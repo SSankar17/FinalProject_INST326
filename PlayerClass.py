9392e5c0d8b3dd718b2bdc0cb146c7325f4e4095
SSankar17 <114683975+SSankar17@users.noreply.github.com>
2023-05-05 11:09:07 -0400

    Added to main
    2 new magic methods -- repr and str

--- PlayerClass.py
+++ PlayerClass.py
@@ -2,14 +2,12 @@ import random
 import seaborn as sns
 import argparse 
 import sys
+import re 
 #crack open sequence unpacking 
 # docstrings
 #start with pdf
 #read a file, magic method 
-space_dict = {"Green": "+300",
-              "Blue": "-200",
-              "Purple": "Add Person",
-              "Orange": "+45"}
+
 
 class Player():
     """Creates an instance of a player
@@ -17,15 +15,17 @@ class Player():
         Name (str) - player name
         Account (float) - amount of money in player's account"""
     
-    def __init__(self, name, account= 100000, position=0):
+    def __init__(self, name, account= 100000, position=0): #Sanjana - regex
         """ initialize a player object with a name, account balance, and position on the board
         Args: self = self, name = player name, account = account balance of 
         player, position = position on board"""
-        self.name = name
-        self.position = position if position <= 20
+        r = re.search("[A-Za-z]{2,25}( [A-Za-z]{2,25})?", name)
+        self.name = r.group(0)
+        
+        self.position = position 
         self.account = account 
         
-    def __add__(self, other):
+    def __add__(self, other): #Lauren - magic methods
         """ add the specified amount to the player's account balance and prints the new balance
         Args: self = self, amount = current amount balance of player
         Returns: statement of player name and account balance
@@ -33,39 +33,45 @@ class Player():
         """  
         if not isinstance(other, Player):
             raise TypeError(f"unsupported operand type for other")
-        self.account + other   
-    
-        
-    def __sub__(self, other):
+        self.account + other.account
+       
+    def __sub__(self, other): #Lauren - magic methods
         """ subtract the specified amount to the player's account balance and prints the new balance
         Args: Args: self = self, amount = current amount balance of player
         Returns: statement of player name and account balance
         """
         if not isinstance(other, Player):
             raise TypeError(f"unsupported operand type for other")
-        self.account - other   
+        self.account - other.account   
     
-    def move(self, dice_roll):
+    def move(self): #Lauren
         """ moves the player by the number space specified by ht e dice roll  and prints the new position
         Args: self = self, dice_role = roll of a six sided die
         Returns: statement of the dice roll and the new position of the player on the board
         """
-        self.position += dice_roll
-        print(f"{self.name}  has moved {dice_roll()} spaces and it is now at a position {self.position}")
+        dice_roll = random.randint(1,6)
+        self.position += dice_roll if self.position+dice_roll <= 20 else "End Game"
+        return self.position
+    
+    def __repr__(self): #Sanjana - magic method
+        return f"{self.name} has {self.account} left and is at position {self.position}"
         
+    def __str__(self): #Sanjana - magic method
+        return f"Player({self.name}, {str(self.account)}, {str(self.position)})"
+
 class Boardgame:
     """create an instance for the boardgame
     Attributes: 
       num players(int)= number of players
     """
-    def __init__(self, num_players):
+    def __init__(self, num_players): #sanjana
         """ initiallizes the number of players
         Args: self=self, num_player = the number of players playing
         """
         self.num_players = num_players
         self.players = []
 
-    def generate_events(self):
+    def generate_events(self): #Glory
         """
         """
         for i in range(0, self.size, 10):
@@ -74,18 +80,18 @@ class Boardgame:
                     player.money += player.career.salary
                     print(f"{player.name} received a pay raise of ${player.career.salary}!")
      
-    def get_winner(self, player_name):
+    def get_winner(self, ): #Sanjana
        scores = []
        name_score = [[i for i, j in self.players], [j for i, j in self.players]]
        name_score.sort(reverse=True)
 
         
-    def dice_roll():
+    def dice_roll(): #Sanjana
         """ the amount of time a player gets to roll the dice
         """
         return random.randint(1,6)
         
-class Car:
+class Car: #sanjana 
     def __init__(self, player_name, num_people=1):
         self.player_name = player_name 
         
@@ -94,19 +100,63 @@ class Car:
 
 
 
-def main(self, filepath):
-    instruction = ""
-    with open(filepath, "r", encoding="utf-8") as f:
-        for space in f:
-            number, color = space.strip().split()
-            if self.position == number:
-                instruction = space_dict[color]
-            if instruction == "Add Person":
-                add_person(player_name)
-               
-                
+def main(self, filepath): #Sanjana 
+    
+    space_dict = {"Green": "+300",
+              "Blue": "-200",
+              "Purple": "Add Person",
+              "Orange": "+45"}
+    
+    players = []
+    
+    while True:
+        try:
+            num_players = int(input("How many players for this game?: "))
+            if num_players == 2:
+                p1 = input("Add a name: ")
+                players.append(Player(p1))
+                p2 = input("Add a name: ")
+                players.append(Player(p2))
+            if num_players == 3:
+                p1 = input("Add a name: ")
+                players.append(Player(p1))
+                p2 = input("Add a name: ")
+                players.append(Player(p2))
+                p3 = input("Add a name: ")
+                players.append(Player(p3))
+            if num_players == 4:
+                p1 = input("Add a name: ")
+                players.append(Player(p1))
+                p2 = input("Add a name: ")
+                players.append(Player(p2))
+                p3 = input("Add a name: ")
+                players.append(Player(p3))
+                p4 = input("Add a name: ")
+                players.append(Player(p4))
+            
+        except:
+            print("Input a number between 2 and 4.")
+        
+        while True: 
+            for x in range(players):
+                players[x].move()
+                print("\nPlayer" + ":",players[x].name)
+                print("Balance: $",players[x].account)
+                print("Position",players[x].position)
+        
+            instruction = ""
+            with open(filepath, "r", encoding="utf-8") as f:
+                for space in f:
+                    number, color = space.strip().split()
+                    for x in players:
+                        if players[x].position == number:
+                            player_space = color
+                        
     print(instruction)
     
+
+
+
 def parse_args(args):
     parser = argparse.ArgumentParser("Run a simplified version of the Life board game.")
     parser.add_argument("filename", type=str,  help="board name")
@@ -119,4 +169,5 @@ if __name__ == "__main__":
     main(args.filepath)
 
                     
+                  
    
